3d00f8d94cba7c1834f28da60b3ec24ebe84cb1a
Olalekan Bisiriyu <olalekanbisiriyu@yahoo.com>
2016-09-14 12:39:03 -0500

    Displayed images and parsed CSV files

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by olalekan bisiriyu on 2016-09-14.
+//  Copyright © 2016 olalekan bisiriyu. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
